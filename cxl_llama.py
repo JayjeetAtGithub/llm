134e64805423b691035847759785d91843e38358
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-02-18 12:17:58 -0800

    Set huggingface cache

--- cxl_llama.py
+++ cxl_llama.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import sys
 import torch
@@ -10,6 +11,8 @@ if __name__ == "__main__":
     # Set up logging
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+    os.environ["HF_HOME"] = "./cache"
+
 
     selected_model = "meta-llama/Llama-2-7b-hf"
 

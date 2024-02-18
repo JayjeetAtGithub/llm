b858bdd065b2554dd4eae1a21eb63fcc47ed26b9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-02-18 12:16:22 -0800

    Add llama

---
+++ cxl_llama.py
@@ -0,0 +1,48 @@
+import logging
+import sys
+import torch
+
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core import PromptTemplate, Settings, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+if __name__ == "__main__":
+    # Set up logging
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+    selected_model = "meta-llama/Llama-2-7b-hf"
+
+    SYSTEM_PROMPT = """You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
+    - Generate human readable output, avoid creating output with gibberish text.
+    - Generate only the requested output, don't include any other language before or after the requested output.
+    - Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
+    - Generate professional language typically used in business documents in North America.
+    - Never generate offensive or foul language.
+    """
+
+    query_wrapper_prompt = PromptTemplate(
+        "[INST]<<SYS>>\n" + SYSTEM_PROMPT + "<</SYS>>\n\n{query_str}[/INST] "
+    )
+
+    llm = HuggingFaceLLM(
+        context_window=4096,
+        max_new_tokens=2048,
+        generate_kwargs={"temperature": 0.0, "do_sample": False},
+        query_wrapper_prompt=query_wrapper_prompt,
+        tokenizer_name=selected_model,
+        model_name=selected_model,
+        device_map="auto",
+        # change these settings below depending on your GPU
+        model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": True},
+    )
+
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+
+    documents = SimpleDirectoryReader("papers_data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What did the author do growing up?")
+    print(response)

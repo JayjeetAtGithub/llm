ce40a5ac0dd2fd2e7914b745abb2e630b9d9ecb5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-02-20 06:53:28 -0800

    Change question

---
+++ rag2.py
@@ -0,0 +1,69 @@
+import os
+import torch
+
+os.environ["HF_HOME"] = os.path.join(os.getcwd(), "huggingface_cache")
+os.environ["HF_TOKEN"] = "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"
+os.environ["LANCEDB_CONFIG_DIR"] = os.path.join(os.getcwd(), "lancedb_config")
+os.environ["PYTORCH_KERNEL_CACHE_PATH"] = os.path.join(os.getcwd(), "pytorch_kernel_cache")
+
+if __name__ == "__main__":
+    from llama_index.core import PromptTemplate, Settings, SimpleDirectoryReader, VectorStoreIndex, StorageContext
+    from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+    from llama_index.vector_stores.lancedb import LanceDBVectorStore
+    from llama_index.llms.llama_cpp import LlamaCPP
+    from llama_index.llms.llama_cpp.llama_utils import (
+        messages_to_prompt,
+        completion_to_prompt,
+    )
+
+    model = "meta-llama/Llama-2-7b-hf"
+
+    SYSTEM_PROMPT = """You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
+    - Generate human readable output, avoid creating output with gibberish text.
+    - Generate only the requested output, don't include any other language before or after the requested output.
+    - Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
+    - Generate professional language typically used in business documents in North America.
+    - Never generate offensive or foul language.
+    """
+
+    prompt = PromptTemplate(
+        "[INST]<<SYS>>\n" + SYSTEM_PROMPT + "<</SYS>>\n\n{query_str}[/INST] "
+    )
+    
+    model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin"
+
+    llm = LlamaCPP(
+        # You can pass in the URL to a GGML model to download it automatically
+        model_url=model_url,
+        # optionally, you can set the path to a pre-downloaded model instead of model_url
+        model_path=None,
+        temperature=0.1,
+        max_new_tokens=256,
+        # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+        context_window=3900,
+        # kwargs to pass to __call__()
+        generate_kwargs={},
+        # kwargs to pass to __init__()
+        # set to at least 1 to use GPU
+        model_kwargs={"n_gpu_layers": 1},
+        # transform inputs into Llama2 format
+        messages_to_prompt=messages_to_prompt,
+        completion_to_prompt=completion_to_prompt,
+        verbose=True,
+    )
+
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+
+    documents = SimpleDirectoryReader("papers_data").load_data()
+
+    vector_store = LanceDBVectorStore(uri="/tmp/lancedb")
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context
+    )
+
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What is a CXL type 3 device ? How is it different from a Type 1 device ?")
+    print(response)

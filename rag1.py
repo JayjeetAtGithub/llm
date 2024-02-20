4e5c522a863eeedf9d34b46c1529d1a0e33eda08
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-02-20 07:49:31 -0800

    Use GPT 3.5

--- rag1.py
+++ rag1.py
@@ -9,25 +9,29 @@ os.environ["OPENAI_API_KEY"] = "sk-w0kZXOlvsAr99HjVHG5DT3BlbkFJo9XirrjfFrmEsVQYb
 
 if __name__ == "__main__":
     from llama_index.llms.huggingface import HuggingFaceLLM
+    from llama_index.llms import OpenAI
     from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex, StorageContext
     from llama_index.embeddings.huggingface import HuggingFaceEmbedding
     from llama_index.vector_stores.lancedb import LanceDBVectorStore
 
-    model = "meta-llama/Llama-2-7b-hf"
+    # model = "meta-llama/Llama-2-7b-hf"
     
-    llm = HuggingFaceLLM(
-        context_window=4096,
-        max_new_tokens=2048,
-        generate_kwargs={"temperature": 0.1},
-        tokenizer_name=model,
-        model_name=model,
-        device_map="auto",
-        model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": False},
-    )
+    # llm = HuggingFaceLLM(
+    #     context_window=4096,
+    #     max_new_tokens=2048,
+    #     generate_kwargs={"temperature": 0.1},
+    #     tokenizer_name=model,
+    #     model_name=model,
+    #     device_map="auto",
+    #     model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": False},
+    # )
 
-    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    # embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    # Settings.embed_model = embed_model
+
+    llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
     Settings.llm = llm
-    Settings.embed_model = embed_model
+
 
     documents = SimpleDirectoryReader("papers_data").load_data()
 

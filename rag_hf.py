b565da2a0f48cf06d500bd7508e7f7b6d3e07600
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-02-20 15:14:32 -0800

    Add comments and code to create cache directories if non-existent

--- rag_hf.py
+++ rag_hf.py
@@ -1,6 +1,17 @@
 import os
 import torch
 
+# Setup the local caches for HuggingFace, LanceDB and PyTorch
+if not os.path.exists("huggingface_cache"):
+    os.makedirs("huggingface_cache")
+
+if not os.path.exists("lancedb_config"):
+    os.makedirs("lancedb_config")
+
+if not os.path.exists("pytorch_kernel_cache"):
+    os.makedirs("pytorch_kernel_cache")
+
+# Set the environment variables for the caches
 os.environ["HF_HOME"] = os.path.join(os.getcwd(), "huggingface_cache")
 os.environ["HF_TOKEN"] = "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"
 os.environ["LANCEDB_CONFIG_DIR"] = os.path.join(os.getcwd(), "lancedb_config")
@@ -13,12 +24,14 @@ if __name__ == "__main__":
     from llama_index.embeddings.huggingface import HuggingFaceEmbedding
     from llama_index.vector_stores.lancedb import LanceDBVectorStore
 
+    # Define the system and query wrapper prompts
     system_prompt = """
         You are a Q/A assistant for a research paper library. Your goal is to answer questions 
         as accurately as possible based on the instructions and context provided.  
     """
     query_wrapper_prompt = SimpleInputPrompt("<|USER|>{query_str}<|ASSISTANT|>")
 
+    # Define the LLM and Embedding models
     model = "meta-llama/Llama-2-7b-chat-hf"
     
     llm = HuggingFaceLLM(
@@ -32,21 +45,22 @@ if __name__ == "__main__":
         device_map="auto",
         model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": False},
     )
-
     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
     
+    # Set the LLM and Embedding models in the settings
     Settings.llm = llm
     Settings.embed_model = embed_model
     Settings.chunk_size = 1024
 
+    # Load the documents and create the index
     documents = SimpleDirectoryReader("papers_data").load_data()
-
     vector_store = LanceDBVectorStore(uri="/tmp/lancedb")
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
     index = VectorStoreIndex.from_documents(
         documents, storage_context=storage_context
     )
 
+    # Query the index
     while True:
         query = str(input("Enter query: "))
         if len(query) > 0:

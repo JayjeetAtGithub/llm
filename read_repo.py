bcd6a90b17156c8f28ce6e7b3a41f5d4321d612f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-03-04 12:34:40 -0800

    Read out kernel source code

---
+++ read_repo.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+
+def read_file(file_path):
+    with open(file_path, "r") as f:
+        contents = f.read()
+        contents = contents.strip()
+        contents = contents.replace("\n", " ")
+        return contents
+
+
+if __name__ == "__main__":
+    kernel_source = "kernel.txt"
+    kernel_file = open(kernel_source, "w")
+
+    for dirpath, dnames, fnames in os.walk("./linux"):
+        for f in fnames:
+            if f.endswith(".c"):
+                contents = read_file(os.path.join(dirpath, f))
+                kernel_file.write(contents)
+            if f.endswith(".h"):
+                contents = read_file(os.path.join(dirpath, f))
+                kernel_file.write(contents)

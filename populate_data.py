d514984212044e9c9f85b5c4a20a8fdfcb0c5846
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-03-06 00:12:53 -0800

    Change download path

--- populate_data.py
+++ populate_data.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("KShivendu/dbpedia-entities-openai-1M")    
+    dataset = load_dataset("KShivendu/dbpedia-entities-openai-1M", cache_dir="hf_dataset")    
     print("Data downloaded successfully")

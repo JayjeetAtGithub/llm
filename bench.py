b1004e60202c7cc983817be3aaeb3654fa3d6c33
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-03-17 10:41:01 -0700

    Measure time for 1000 queries

--- bench.py
+++ bench.py
@@ -231,7 +231,6 @@ if __name__ == "__main__":
                 queries_ran += 1
                 if queries_ran >= config["queries_to_run"]:
                     break
-        
+                
         print(f"[INFO] Total time for {config["queries_to_run"]} queries: {total_time_for_queries} seconds")
-
     print("[INFO] Done!")

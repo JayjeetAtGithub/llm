3079bce21f2d48be9c225e5b81098829f1b73671
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-10 16:46:28 -0700

    Comment out manual steps

--- scripts/install_pgvector.sh
+++ scripts/install_pgvector.sh
@@ -18,8 +18,10 @@ sudo make install
 sudo adduser postgres
 sudo mkdir -p /mnt/workspace/pgsql/data
 sudo chown postgres /mnt/workspace/pgsql/data
-su - postgres
-/usr/local/pgsql/bin/initdb -D /mnt/workspace/pgsql/data
-/usr/local/pgsql/bin/pg_ctl -D /mnt/workspace/pgsql/data -l logfile start
-/usr/local/pgsql/bin/createdb test
-/usr/local/pgsql/bin/psql test
+
+# manual steps
+# su - postgres
+# /usr/local/pgsql/bin/initdb -D /mnt/workspace/pgsql/data
+# /usr/local/pgsql/bin/pg_ctl -D /mnt/workspace/pgsql/data -l logfile start
+# /usr/local/pgsql/bin/createdb test
+# /usr/local/pgsql/bin/psql test

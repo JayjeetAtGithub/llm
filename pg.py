797a729fc76f0ec7f09da48f61ed3342b971b4c6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-10 19:29:10 -0700

    perform search

--- pg.py
+++ pg.py
@@ -42,10 +42,13 @@ if __name__ == "__main__":
         print(f"Inserted {row_idx} rows into pg_vector")
 
     if args.query:
+        query_idx = 0
         file_list = os.listdir("dbpedia-entities-openai-1M/data")[5:15]
         for file in file_list:
             batch = read_parquet_file(os.path.join("dbpedia-entities-openai-1M/data", file))
             for row in batch:
                 embedding = ','.join([str(x) for x in row[3]])
                 conn.execute(f"SELECT * FROM embeddings_table ORDER BY embedding <-> '[{embedding}]' LIMIT 5;").fetchall()
-                print("Ran query on pg_vector")
+                print(f"Ran query {query_idx} on pg_vector")
+                query_idx += 1
+

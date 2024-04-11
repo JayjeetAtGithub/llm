fcf7d6b80404b8127c44f6ff61ebccb59d5257fa
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-10 18:21:25 -0700

    Insert data into pg_vector

--- insert_pg.py
+++ insert_pg.py
@@ -13,17 +13,24 @@ def read_parquet_file(file_path):
 
 if __name__ == "__main__":
 
-    conn = psycopg.connect(dbname='vectordb', autocommit=True)
+    conn = psycopg.connect("postgresql://postgres:postgres@localhost:5432/vectordb", autocommit=True)
+    print("Connected to PostgreSQL: ", conn)
 
     conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
     register_vector(conn)
+    print("Initiated extension: pg_vector")
 
     conn.execute('DROP TABLE IF EXISTS embeddings_table')
     conn.execute('CREATE TABLE embeddings_table (id bigserial PRIMARY KEY, content text, embedding vector(1536))')
+    print("Created table: embeddings_table")
 
     file_list = os.listdir("dbpedia-entities-openai-1M/data")
+    row_idx = 0
     for file in file_list:
         batch = read_parquet_file(os.path.join("dbpedia-entities-openai-1M/data", file))
         for row in batch:
             conn.execute('INSERT INTO documents (content, embedding) VALUES (%s, %s)', (row[2], row[3]))        
-            print(f"Inserted row into pg_vector")
+            print(f"Inserted row {row_idx} into pg_vector")
+            row_idx += 1
+
+    print(f"Inserted {row_idx} rows into pg_vector")

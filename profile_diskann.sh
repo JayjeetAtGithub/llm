88246e2c340aa5dc0eb50d9b98fbaea6816b2fc9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-15 13:47:52 -0700

    Add profile script

--- profile_diskann.sh
+++ profile_diskann.sh
@@ -11,7 +11,8 @@ DISKANN_DATA_GIST_QUERY=${DISKANN_DATA}/gist/gist_query.fvecs
 DISKANN_DATA_GIST_BASE_FBIN=${DISKANN_DATA}/gist/gist_base.fbin
 DISKANN_DATA_GIST_LEARN_FBIN=${DISKANN_DATA}/gist/gist_learn.fbin
 DISKANN_DATA_GIST_QUERY_FBIN=${DISKANN_DATA}/gist/gist_query.fbin
-DISKANN_DATA_GIST_GROUNDTRUTH=${DISKANN_DATA}/gist/gist_groundtruth
+DISKANN_DATA_GIST_GROUNDTRUTH_1=${DISKANN_DATA}/gist/gist_groundtruth_1
+DISKANN_DATA_GIST_GROUNDTRUTH_2=${DISKANN_DATA}/gist/gist_groundtruth_2
 DISKANN_DATA_GIST_INDEX=${DISKANN_DATA}/gist/gist_index
 DISKANN_DATA_GIST_RES=${DISKANN_DATA}/gist/gist_res
 
@@ -22,7 +23,8 @@ DISKANN_DATA_SIFT_QUERY=${DISKANN_DATA}/sift/sift_query.fvecs
 DISKANN_DATA_SIFT_BASE_FBIN=${DISKANN_DATA}/sift/sift_base.fbin
 DISKANN_DATA_SIFT_LEARN_FBIN=${DISKANN_DATA}/sift/sift_learn.fbin
 DISKANN_DATA_SIFT_QUERY_FBIN=${DISKANN_DATA}/sift/sift_query.fbin
-DISKANN_DATA_SIFT_GROUNDTRUTH=${DISKANN_DATA}/sift/sift_groundtruth
+DISKANN_DATA_SIFT_GROUNDTRUTH_1=${DISKANN_DATA}/sift/sift_groundtruth_1
+DISKANN_DATA_SIFT_GROUNDTRUTH_2=${DISKANN_DATA}/sift/sift_groundtruth_2
 DISKANN_DATA_SIFT_INDEX=${DISKANN_DATA}/sift/sift_index
 DISKANN_DATA_SIFT_RES=${DISKANN_DATA}/sift/sift_res
 
@@ -39,6 +41,8 @@ if [ ! -d "${DISKANN_DATA_SIFT_QUERY_FBIN}" ]; then
     ${DISKANN_HOME}/build/apps/utils/fvecs_to_bin float ${DISKANN_DATA_SIFT_QUERY} ${DISKANN_DATA_SIFT_QUERY_FBIN}
 fi
 
-${DISKANN_HOME}/build/apps/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file ${DISKANN_DATA_SIFT_LEARN_FBIN}  --query_file  ${DISKANN_DATA_SIFT_QUERY_FBIN} --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH} --K 100
+${DISKANN_HOME}/build/apps/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file ${DISKANN_DATA_SIFT_LEARN_FBIN}  --query_file  ${DISKANN_DATA_SIFT_QUERY_FBIN} --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_1} --K 100
+${DISKANN_HOME}/build/apps/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file ${DISKANN_DATA_SIFT_LEARN_FBIN}  --query_file  ${DISKANN_DATA_SIFT_BASE_FBIN} --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_2} --K 100
+
 ${DISKANN_HOME}/build/apps/build_memory_index  --data_type float --dist_fn l2 --data_path ${DISKANN_DATA_SIFT_LEARN_FBIN} --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} -R 32 -L 50 --alpha 1.2
-${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} --query_file ${DISKANN_DATA_SIFT_QUERY_FBIN}  --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH} -K 10 -L 10 20 30 40 50 100 -T 1 --result_path ${DISKANN_DATA_SIFT_RES}
+${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} --query_file ${DISKANN_DATA_SIFT_QUERY_FBIN}  --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_1} -K 10 -L 10 20 30 40 50 100 -T 1 --result_path ${DISKANN_DATA_SIFT_RES}

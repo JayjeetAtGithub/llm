dbe1fc533da9f8d31d0557491f0dd1cdbdf5d203
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-15 13:50:33 -0700

    Add profile script

--- profile_diskann.sh
+++ profile_diskann.sh
@@ -46,4 +46,4 @@ ${DISKANN_HOME}/build/apps/utils/compute_groundtruth  --data_type float --dist_f
 ${DISKANN_HOME}/build/apps/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file ${DISKANN_DATA_SIFT_LEARN_FBIN}  --query_file  ${DISKANN_DATA_SIFT_BASE_FBIN} --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_2} --K 100
 
 ${DISKANN_HOME}/build/apps/build_memory_index  --data_type float --dist_fn l2 --data_path ${DISKANN_DATA_SIFT_LEARN_FBIN} --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} -R 32 -L 50 --alpha 1.2
-${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} --query_file ${DISKANN_DATA_SIFT_QUERY_FBIN}  --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_1} -K 10 -L 10 20 30 40 50 100 -T 1 --result_path ${DISKANN_DATA_SIFT_RES}
+${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} --query_file ${DISKANN_DATA_SIFT_BASE_FBIN}  --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_2} -K 10 -L 10 20 30 40 50 100 -T 1 --result_path ${DISKANN_DATA_SIFT_RES}

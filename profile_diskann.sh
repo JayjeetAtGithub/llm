93582c5d12187979999a8fcec427b05a6723b3e5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-15 11:14:05 -0700

    Add profile script

--- profile_diskann.sh
+++ profile_diskann.sh
@@ -7,6 +7,5 @@ DISKANN_DATA=${DISKANN_HOME}/data
 ${DISKANN_HOME}/build/apps/utils/fvecs_to_bin float ${DISKANN_DATA}/gist/gist_learn.fvecs ${DISKANN_DATA}/gist/gist_learn.fbin
 ${DISKANN_HOME}/build/apps/utils/fvecs_to_bin float ${DISKANN_DATA}/gist/gist_query.fvecs ${DISKANN_DATA}/gist/gist_query.fbin
 
-${DISKANN_HOME}/build/apps/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file ${DISKANN_DATA}/gist/gist_learn.fbin --query_file  ${DISKANN_DATA}/gist/gist_query.fbin --gt_file ${DISKANN_DATA}/gist/gist_groundtruth.ivecs --K 100
 ${DISKANN_HOME}/build/apps/build_memory_index  --data_type float --dist_fn l2 --data_path ${DISKANN_DATA}/gist/gist_learn.fbin --index_path_prefix ${DISKANN_DATA}/gist/gist_index -R 32 -L 50 --alpha 1.2
 ${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA}/gist/gist_index --query_file ${DISKANN_DATA}/gist/gist_query.fbin  --gt_file ${DISKANN_DATA}/gist/gist_groundtruth.ivecs -K 10 -L 10 20 30 40 50 100 --result_path ${DISKANN_DATA}/gist/res

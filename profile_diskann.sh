be0a46bbea60e2e2f94a9574cb2bc3b0580021e5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-15 12:02:13 -0700

    Add profile script

--- profile_diskann.sh
+++ profile_diskann.sh
@@ -39,4 +39,4 @@ if [ ! -d "${DISKANN_DATA_SIFT_GROUNDTRUTH_BIN}" ]; then
 fi
 
 ${DISKANN_HOME}/build/apps/build_memory_index  --data_type float --dist_fn l2 --data_path ${DISKANN_DATA_SIFT_LEARN_FBIN} --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} -R 32 -L 50 --alpha 1.2
-${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} --query_file ${DISKANN_DATA_SIFT_QUERY_FBIN}  --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_BIN} -K 10 -L 10 20 30 40 50 100 --result_path ${DISKANN_DATA_SIFT_RES}
+${DISKANN_HOME}/build/apps/search_memory_index  --data_type float --dist_fn l2 --index_path_prefix ${DISKANN_DATA_SIFT_INDEX} --query_file ${DISKANN_DATA_SIFT_QUERY_FBIN}  --gt_file ${DISKANN_DATA_SIFT_GROUNDTRUTH_BIN} -K 10 -L 10 20 30 40 50 100 -T 1 --result_path ${DISKANN_DATA_SIFT_RES}

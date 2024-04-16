df08c3a66b9e8ee6f3fb43571e66efa7e6298189
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-16 12:05:35 -0700

    Add basic faiss code

--- faiss/profile_faiss.cc
+++ faiss/profile_faiss.cc
@@ -4,6 +4,22 @@
 
 
 int main() {
+    int d = 64;                            // dimension
+    int nb = 100000;                       // database size
+    int nq = 10000;                        // nb of queries
+    float *xb = new float[d * nb];
+    float *xq = new float[d * nq];
+    
+    for(int i = 0; i < nb; i++) {
+        for(int j = 0; j < d; j++) xb[d * i + j] = drand48();
+        xb[d * i] += i / 1000.;
+    }
+    
+    for(int i = 0; i < nq; i++) {
+        for(int j = 0; j < d; j++) xq[d * i + j] = drand48();
+        xq[d * i] += i / 1000.;
+    }
+
     faiss::IndexFlatL2 index(d);
     printf("is_trained = %s\n", index.is_trained ? "true" : "false");
     printf("ntotal = %ld\n", index.ntotal);

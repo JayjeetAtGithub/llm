04116fbaee747e63fd1765144acb3b841d66cd64
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-18 16:53:26 -0700

    Changes

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import numpy as np
+import struct
 
-fname = "faiss/siftsmall/siftsmall_base.fvecs"
+def fvecs_read(filename, c_contiguous=True):
+    with open(filename, 'rb') as f:
+        x = f.read(4)
+        int_val = int.from_bytes(x, "little")
+        print(int_val)
 
-def ivecs_read(fname):
-    a = np.fromfile(fname, dtype='int32')
-    d = a[0]
-    return a.reshape(-1, d + 1)[:, 1:].copy()
+        a = f.read(4)
+        val = struct.unpack('f', a)
+        print(val)
 
+        b = f.read(4)
+        val = struct.unpack('f', b)
+        print(val)
+    # print(fv)
+    # if fv.size == 0:
+    #     return np.zeros((0, 0))
+    # dim = fv.view(np.int32)[0]
+    # assert dim > 0
+    # fv = fv.reshape(-1, 1 + dim)
+    # if not all(fv.view(np.int32)[:, 0] == dim):
+    #     raise IOError("Non-uniform vector sizes in " + filename)
+    # fv = fv[:, 1:]
+    # if c_contiguous:
+    #     fv = fv.copy()
+    # return fv
 
-def fvecs_read(fname):
-    return ivecs_read(fname).view('float32')
-
-print(fvecs_read(fname))
+out = fvecs_read('faiss/siftsmall/siftsmall_base.fvecs')
+print(out)

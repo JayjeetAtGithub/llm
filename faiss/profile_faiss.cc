8ef53d22fe43f3c4487b64d16ead1b4d8a346776
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-18 12:32:55 -0700

    Save indeX

--- faiss/profile_faiss.cc
+++ faiss/profile_faiss.cc
@@ -9,6 +9,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIVFFlat.h>
@@ -36,38 +37,51 @@ std::shared_ptr<faiss::Index> create_index(int index_id, size_t dim) {
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        std::cout << "usage: " << argv[0] << " [index_id] " << "[dataset]" << std::endl;
+        std::cout << "usage: " << argv[0] << " [index_id] " << "[dataset] " << "[operation]" << std::endl;
         exit(1);
     }
 
     int index_id = std::stoi(argv[1]);
     std::string dataset = argv[2];
+    std::string operation = argv[3];
     std::cout << "Using index: " << index_id_to_name(index_id) << "\n";
     std::cout << "Using dataset: " << dataset << std::endl;
+    std::cout << "Performing operation: " << operation << std::endl;
     
-    size_t dim_learn, n_learn;
-    float* data_learn;
-    std::string dataset_path_learn = dataset + "/" + dataset + "_base.fvecs";
-    read_dataset(dataset_path_learn.c_str(), data_learn, &dim_learn, &n_learn);
-    std::cout << "Read in learn dataset " << dim_learn << " x " << n_learn << std::endl;
-    preview_dataset(data_learn);
+    if (operation == "index") {
+        size_t dim_learn, n_learn;
+        float* data_learn;
+        std::string dataset_path_learn = dataset + "/" + dataset + "_base.fvecs";
+        read_dataset(dataset_path_learn.c_str(), data_learn, &dim_learn, &n_learn);
+        std::cout << "Read in learn dataset " << dim_learn << " x " << n_learn << std::endl;
+        preview_dataset(data_learn);
 
-    size_t dim_query, n_query;
-    float* data_query;
-    std::string dataset_path_query = dataset + "/" + dataset + "_query.fvecs";
-    read_dataset(dataset_path_query.c_str(), data_query, &dim_query, &n_query);
-    std::cout << "Read in query dataset " << dim_query << " x " << n_query << std::endl;
-    preview_dataset(data_query);
-
-
-    std::shared_ptr<faiss::Index> index = create_index(index_id, dim_learn);
-    if (index_id == 1) {
-        index->train(n_learn, data_learn);
+        std::shared_ptr<faiss::Index> index = create_index(index_id, dim_learn);
+        if (index_id == 1) {
+            index->train(n_learn, data_learn);
+        }
+        index->add(n_learn, data_learn);
+        std::string index_path =  "index." + index_id_to_name(index_id) + ".faiss";
+        write_index(index.get(), index_path.c_str());
+        delete data_learn;
     }
-    index->add(n_learn, data_learn);
-    std::string index_path =  "index." + index_id_to_name(index_id) + ".faiss";
-    write_index(index.get(), index_path.c_str());
-    delete data_learn;
+
+    if (operation == "query") {
+        size_t dim_query, n_query;
+        float* data_query;
+        std::string dataset_path_query = dataset + "/" + dataset + "_query.fvecs";
+        read_dataset(dataset_path_query.c_str(), data_query, &dim_query, &n_query);
+        std::cout << "Read in query dataset " << dim_query << " x " << n_query << std::endl;
+        preview_dataset(data_query);
+
+        std::vector<faiss::idx_t> nns(TOP_K * n_query);
+        std::vector<float> dis(TOP_K * n_query);
+
+        Index* index = read_index(index_path.c_str());
+        index.search(n_query, data_query, TOP_K, dis.data(), nns.data());
+
+        delete data_query;
+    }
 
 
 

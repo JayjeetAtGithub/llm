a525e26fe8de7b7bd53f4f0b54a64ac627fb38cc
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-18 12:57:24 -0700

    rename script

--- faiss/utils.h
+++ faiss/utils.h
@@ -49,7 +49,7 @@ int* ivecs_read(const char* fname, size_t* d_out, size_t* n_out) {
 void preview_dataset(float* xb) {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 10; j++) {
-            std::cout << xb[i * 10 + j] << " ";
+            printf("%g ", xb[i * 10 + j]);
         }
         std::cout << std::endl;
     }

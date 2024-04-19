2a751422b69e46c2872cb3d5ac5a2f21b1758c71
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-19 12:03:52 -0700

    Build cmake

---
+++ scripts/install_cmake.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+wget https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2.tar.gz /mnt/workspace/cmake-3.29.2.tar.gz
+tar -xvzf /mnt/workspace/cmake-3.29.2.tar.gz
+
+cd /mnt/workspace/cmake-3.29.2
+./bootstrap
+sudo make -j$(nproc) install

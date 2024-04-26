dcd1e601e537deccbb98ca07f4b785366b26dffb
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-04-26 09:46:26 -0700

    Fix wget script

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ex
 
-rm -rf /mnt/workspace/cmake-3.29.2*
-wget https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2.tar.gz -O /mnt/workspace/cmake-3.29.2.tar.gz
-tar -xvzf /mnt/workspace/cmake-3.29.2.tar.gz
+cd /mnt/workspace
+rm -rf cmake-3.29.2*
+wget https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2.tar.gz
+tar -xvzf cmake-3.29.2.tar.gz
 
-cd /mnt/workspace/cmake-3.29.2
+cd cmake-3.29.2
 ./bootstrap
 sudo make -j$(nproc) install

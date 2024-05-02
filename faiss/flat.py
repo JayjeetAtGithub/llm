bc0a767472499b7e818f6ae17d71abba15c0e513
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-01 21:13:56 -0700

    Set dimension to 8

--- faiss/flat.py
+++ faiss/flat.py
@@ -18,7 +18,6 @@ print(xq)
 
 import faiss                   # make faiss available
 index = faiss.IndexFlatL2(d)   # build the index
-print(index.is_trained)
 index.add(xb)                  # add vectors to the index
 print(index.ntotal)
 
@@ -26,6 +25,7 @@ k = 4                          # we want to see 4 nearest neighbors
 D, I = index.search(xb[:5], k) # sanity check
 print(I)
 print(D)
+
 D, I = index.search(xq, k)     # actual search
 print(I[:5])                   # neighbors of the 5 first queries
 print(I[-5:])                  # neighbors of the 5 last queries

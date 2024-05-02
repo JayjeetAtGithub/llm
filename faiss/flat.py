720d97d1da33fa2b0e253e9297c3aed85da75ff9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-02 00:33:43 -0700

    Set dimension to 8

--- faiss/flat.py
+++ faiss/flat.py
@@ -2,7 +2,7 @@ import numpy as np
 
 d = 1536                      # dimension
 nb = 1000000                     # database size
-nq = 100000                       # nb of queries
+nq = 10000                       # nb of queries
 np.random.seed(1234)             # make reproducible
 xb = np.random.random((nb, d)).astype('float32')
 xb[:, 0] += np.arange(nb) / 1000.

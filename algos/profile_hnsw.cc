a68604f8b91bd6e68b1c2ae87a5907fa02252512
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-02 14:23:14 -0700

    Print the top

--- algos/profile_hnsw.cc
+++ algos/profile_hnsw.cc
@@ -1,11 +1,10 @@
 #include "lib/hnswlib.h"
 #include "utils.h"
 
-#define TOP_K 10
-
 int main(int argc, char **argv) {
     std::string dataset = argv[1];
     std::string operation = argv[2];
+    int top_k = std::stoi(argv[3]);
     print_pid();
 
     std::cout << "Using dataset: " << dataset << std::endl;
@@ -14,32 +13,33 @@ int main(int argc, char **argv) {
     int M = 32;
     int ef_construction = 200;
 
-    size_t dim, n;
-    float* data;
+    // Learn Dataset
+    size_t dim_learn, n_learn;
+    float* data_learn;
     std::string dataset_path_learn = dataset + "/" + dataset + "_base.fvecs";
-    read_dataset(dataset_path_learn.c_str(), data, &dim, &n);
+    read_dataset(dataset_path_learn.c_str(), data_learn, &dim_learn, &n_learn);
+    std::cout << "Learn dataset shape: " << dim_learn << " x " << n_learn << std::endl;
 
-    std::cout << "Dimension: " << dim << std::endl;
-    std::cout << "Num Vectors: " << n << std::endl;
+    // Query Dataset
+    size_t dim_query, n_query;
+    float* data_query;
+    std::string dataset_path_query = dataset + "/" + dataset + "_learn.fvecs";
+    read_dataset(dataset_path_query.c_str(), data_query, &dim_query, &n_query);
+    std::cout << "Query dataset shape: " << dim_query << " x " << n_query << std::endl;
 
-    hnswlib::L2Space space(dim);
-    hnswlib::HierarchicalNSW<float>* alg_hnsw = new hnswlib::HierarchicalNSW<float>(&space, n, M, ef_construction);
+    hnswlib::L2Space space(dim_learn);
+    hnswlib::HierarchicalNSW<float>* alg_hnsw = new hnswlib::HierarchicalNSW<float>(&space, n_learn, M, ef_construction);
 
     for (int i = 0; i < n; i++) {
-        alg_hnsw->addPoint(data + i * dim, i);
+        alg_hnsw->addPoint(data_learn + i * dim_learn, i);
     }
 
-    float correct = 0;
     for (int i = 0; i < n; i++) {
-        std::priority_queue<std::pair<float, hnswlib::labeltype>> result = alg_hnsw->searchKnn(data + i * dim, TOP_K);
-        std::cout << result.top().second << std::endl;
-        hnswlib::labeltype label = result.top().second;
-        if (label == i) correct++;
+        std::priority_queue<std::pair<float, hnswlib::labeltype>> result = alg_hnsw->searchKnn(data_query + i * dim_query, top_k);
     }
-    float recall = correct / n;
-    std::cout << "Recall@" << TOP_K << ": " << recall << "\n";
 
-    delete[] data;
+    delete[] data_learn;
+    delete[] data_query;
     delete alg_hnsw;
     return 0;
 }

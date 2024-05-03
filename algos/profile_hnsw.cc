530c4dcbe2c0df7e36111605a0041e02192d4bcc
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-03 11:44:23 -0700

    Perform brute force indexing

--- algos/profile_hnsw.cc
+++ algos/profile_hnsw.cc
@@ -19,25 +19,43 @@ int main(int argc, char **argv) {
         std::string dataset_path_learn = dataset + "/" + dataset + "_base.fvecs";
         read_dataset(dataset_path_learn.c_str(), data_learn, &dim_learn, &n_learn);
         std::cout << "[INFO] learn dataset shape: " << dim_learn << " x " << n_learn << std::endl;
-
+        
+        std::cout << "[INFO] performing hnsw indexing" << std::endl;
         hnswlib::L2Space space(dim_learn);
         hnswlib::HierarchicalNSW<float>* alg_hnsw = new hnswlib::HierarchicalNSW<float>(&space, n_learn, M, ef_construction);
         
-        std::cout << "[INFO] adding vectors to index" << std::endl;
-
         auto s = std::chrono::high_resolution_clock::now();
         #pragma omp parallel for
         for (int i = 0; i < n_learn; i++) {
             alg_hnsw->addPoint(data_learn + i * dim_learn, i);
         }
         auto e = std::chrono::high_resolution_clock::now();
-        std::cout << "[TIME] index: " << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count() << " ms" << std::endl;
+        std::cout << "[TIME] hnsw_index: " << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count() << " ms" << std::endl;
 
         std::string hnsw_path = "index." + dataset + ".hnswlib";
         alg_hnsw->saveIndex(hnsw_path);
-        std::cout << "[FILESIZE] index_size: " << filesize(hnsw_path.c_str()) << " bytes" << std::endl;
+        std::cout << "[FILESIZE] hnsw_index_size: " << alg_hnsw->indexFileSize() << " bytes" << std::endl;
         
         delete alg_hnsw;
+
+        // now doing brute force search
+        std::cout << "[INFO] performing brute force indexing" << std::endl;
+        hnswlib::BruteForceSearch<float>* alg_brute = new hnswlib::BruteForceSearch<float>(&space);
+
+        s = std::chrono::high_resolution_clock::now();
+        #pragma omp parallel for
+        for (int i = 0; i < n_query; i++) {
+            alg_brute->addPoint(data_learn + i * dim_learn);
+        }
+        e = std::chrono::high_resolution_clock::now();
+        std::cout << "[TIME] brute_force_index: " << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count() << " ms" << std::endl;
+
+        std::string brute_path = "index." + dataset + ".bruteforce";
+        alg_brute->saveIndex(brute_path);
+        std::cout << "[FILESIZE] brute_force_index_size: " << alg_brute->indexFileSize() << " bytes" << std::endl;
+
+        delete alg_brute;
+        
         delete[] data_learn;
     }
 
@@ -59,6 +77,15 @@ int main(int argc, char **argv) {
         }
         auto e = std::chrono::high_resolution_clock::now();
         std::cout << "[TIME] query: " << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count() << " ms" << std::endl;
+
+       
+        s = std::chrono::high_resolution_clock::now();
+        #pragma omp parallel for
+        for (int i = 0; i < n_query; i++) {
+            std::priority_queue<std::pair<float, hnswlib::labeltype>> result = alg_hnsw->searchKnn(data_query + i * dim_query, top_k);
+        }
+        e = std::chrono::high_resolution_clock::now();
+
 
         delete alg_hnsw;
         delete[] data_query;

aa382aa710fc70aa9c14b65881b6ea9b2849df96
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-04 20:37:41 -0700

    Create index

--- algos/profile_faiss.cc
+++ algos/profile_faiss.cc
@@ -73,7 +73,7 @@ int main(int argc, char** argv) {
         preview_dataset(data_query);
 
         if (index == "flat") {
-            n_query = 10;
+            n_query = 100;
         }
 
         std::vector<faiss::idx_t> nns(top_k * n_query);

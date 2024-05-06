22c03bf23b018e83fcebebd829113c5307877f97
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-05 19:22:27 -0700

    Create index

--- algos/create_indexes.sh
+++ algos/create_indexes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 ./bin/profile_faiss flat gist index 10 debug
 ./bin/profile_faiss hnsw gist index 10 debug
 ./bin/profile_hnswlib flat gist index 10 debug

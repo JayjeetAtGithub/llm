c7d5dbdbc3dd2c8cd7261d7841a1163ddd065a22
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-07 14:15:18 -0700

    Add parallelism bench

--- algos/run_parallelism_bench.py
+++ algos/run_parallelism_bench.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 if __name__ == "__main__":
-    threads = [40, 32, 16, 8, 4, 2, 1]
+    threads = [32, 16, 8, 4, 2, 1]
     for thread in threads:
         os.environ["OMP_NUM_THREADS"] = str(thread)
         output = subprocess.run(["./bin/profile_hnswlib", "hnsw", "gist", "query", "10", "debug"], capture_output=True)

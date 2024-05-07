d42e8ed2a425be3bbab783bdfe8da692850bc497
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-07 16:49:14 -0700

    Initial Commit[1;2A

--- algos/run_parallelism_bench.py
+++ algos/run_parallelism_bench.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
             os.environ["OMP_NUM_THREADS"] = str(thread)
             output = subprocess.run(["./bin/profile_hnswlib", "hnsw", "gist", mode, "10", "debug"], capture_output=True)
             print(output.stdout)
-            time = output.stdout.splitlines()
+            time = output.stdout.splitlines()[-2]
             print(time)
-            time_int = int(time[0].decode("utf-8").split()[2]) # ms
-            print(f"Thread: {thread}, Time: {time_int}")
-            data.append({
-                "thread": thread,
-                "time": time_int
-            })
+            # time_int = int(time[0].decode("utf-8").split()[2]) # ms
+    #         print(f"Thread: {thread}, Time: {time_int}")
+    #         data.append({
+    #             "thread": thread,
+    #             "time": time_int
+    #         })
 
-    print(data)
-    with open(f"hnswlib_parallelism_{mode}.json", "w") as f:
-        json.dump(data, f)
+    # print(data)
+    # with open(f"hnswlib_parallelism_{mode}.json", "w") as f:
+    #     json.dump(data, f)

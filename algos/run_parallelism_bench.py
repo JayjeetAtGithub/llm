b274a90ab36d4d3fc50cc924abeee8562ce49774
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-07 14:14:53 -0700

    Add parallelism bench

--- algos/run_parallelism_bench.py
+++ algos/run_parallelism_bench.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         os.environ["OMP_NUM_THREADS"] = str(thread)
         output = subprocess.run(["./bin/profile_hnswlib", "hnsw", "gist", "query", "10", "debug"], capture_output=True)
         time = output.stdout.splitlines()[-1:]
-        print(time)
+        print(time[0].decode("utf-8").split()[2])
 
 
 

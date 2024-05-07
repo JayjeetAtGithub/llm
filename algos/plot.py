68f993f665afb9c9d27ec40933108fa8e3052d3b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-07 16:28:22 -0700

    Add results and plot scripts

---
+++ algos/plot.py
@@ -0,0 +1,19 @@
+import json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    sns.set_style("whitegrid")
+    with open("hnsw_parallelism_query.json", "r") as f:
+        data = json.loads(f.read())
+        print(data)
+
+        df = pd.DataFrame(data)
+
+        sns.barplot(x="thread", y="time", data=df, errorbar="sd", err_kws={'linewidth': 1.5}, capsize=.1)
+        plt.xlabel("No. of Threads")
+        plt.ylabel("Time (ms)")
+        plt.title("Search Duration vs Parallelism")
+        plt.savefig("hnsw_parallelism_query.png")

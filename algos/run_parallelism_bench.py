0e34e57861f0685be155bbe241fd73e7634c8904
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-07 14:23:29 -0700

    Add parallelism bench

--- algos/run_parallelism_bench.py
+++ algos/run_parallelism_bench.py
@@ -6,12 +6,13 @@ if __name__ == "__main__":
     data = {}
     threads = [32, 16]
     for thread in threads:
-        os.environ["OMP_NUM_THREADS"] = str(thread)
-        output = subprocess.run(["./bin/profile_hnswlib", "hnsw", "gist", "query", "10", "debug"], capture_output=True)
-        time = output.stdout.splitlines()[-1:]
-        time_int = int(time[0].decode("utf-8").split()[2]) # ms
-        if thread not in data:
-            data[thread] = []
-        data[thread].append(time_int)
+        for _ in range(3):
+            os.environ["OMP_NUM_THREADS"] = str(thread)
+            output = subprocess.run(["./bin/profile_hnswlib", "hnsw", "gist", "query", "10", "debug"], capture_output=True)
+            time = output.stdout.splitlines()[-1:]
+            time_int = int(time[0].decode("utf-8").split()[2]) # ms
+            if thread not in data:
+                data[thread] = []
+            data[thread].append(time_int)
     
     print(data)

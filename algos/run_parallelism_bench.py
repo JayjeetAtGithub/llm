f8032f231b704c9d965a90c8f3c2dcf83d14b068
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-05-08 17:41:23 -0700

    Fixes

--- algos/run_parallelism_bench.py
+++ algos/run_parallelism_bench.py
@@ -14,9 +14,7 @@ if __name__ == "__main__":
         for _ in range(3):
             os.environ["OMP_NUM_THREADS"] = str(thread)
             output = subprocess.run(["./bin/profile_hnswlib", "hnsw", "gist", mode, "10", "debug"], capture_output=True)
-            print(output.stdout)
             time = output.stdout.splitlines()[-2]
-            print(time)
             time_int = int(time.decode("utf-8").split()[2]) # ms
             print(f"Thread: {thread}, Time: {time_int}")
             data.append({

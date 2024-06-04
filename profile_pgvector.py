c48fe2250cbfbb5218d0fa9019cee00f679142f3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-06-04 02:19:32 -0700

    Fix pgvector script

--- profile_pgvector.py
+++ profile_pgvector.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     conn = psycopg2.connect("postgresql://noobjc@localhost:5432/vectordb")
+    conn.set_session(autocommit=True)
     cursor = conn.cursor()
     print("Connected to PostgreSQL: ", conn)
 

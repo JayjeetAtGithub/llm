371193330b34f36a02cbc914f110471242758893
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-06-03 17:58:36 -0700

    Clean up

--- algos/create_indexes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-./bin/profile_faiss flat gist index 10 debug
-./bin/profile_faiss hnsw gist index 10 debug
-./bin/profile_hnswlib flat gist index 10 debug
-./bin/profile_hnswlib hnsw gist index 10 debug
